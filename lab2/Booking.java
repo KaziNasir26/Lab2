39e990d0408e74819a22af53c73cd6a3fc702649
Kazi Fahad Bin Nasir <kazifahadbin.nasir@ucalgary.ca>
2024-07-11 19:38:52 -0600

    Added booking class

---
+++ lab2/Booking.java
@@ -0,0 +1,17 @@
+package lab2;
+
+public class Booking extends Entity {
+    private int clientId;
+    private int petId;
+    private String startDate;
+    private String endDate;
+
+    public Booking(int id, int clientId, int petId, String startDate, String endDate) {
+        setId(id);
+        this.clientId = clientId;
+        this.petId = petId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+
