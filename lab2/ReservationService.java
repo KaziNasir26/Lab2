9cdffd3850b250ee866b7910a5ac8e829899f772
Kazi Fahad Bin Nasir <kazifahadbin.nasir@ucalgary.ca>
2024-07-11 19:59:46 -0600

    Update

---
+++ lab2/ReservationService.java
@@ -0,0 +1,24 @@
+package lab2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReservationService {
+    private List<Reservation> reservations = new ArrayList<>();
+
+    public void addReservation(Reservation reservation) {
+        reservations.add(reservation);
+    }
+
+    public void updateReservation(Reservation reservation) {
+        for (int i = 0; i < reservations.size(); i++) {
+            if (reservations.get(i).getId() == reservation.getId()) {
+                reservations.set(i, reservation);
+                break;
+            }
+        }
+    }
+
+
+    }
+}
