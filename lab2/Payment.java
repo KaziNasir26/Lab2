223f270934035e787f40709b63563edc08003d1e
Kazi Fahad Bin Nasir <kazifahadbin.nasir@ucalgary.ca>
2024-07-11 22:28:18 -0600

    updating

---
+++ lab2/Payment.java
@@ -0,0 +1,48 @@
+package lab2;
+
+public class Payment extends Entity {
+    private int billingId;
+    private double amountPaid;
+    private String paymentDate;
+    private String paymentMethod;
+
+    public Payment(int id, int billingId, double amountPaid, String paymentDate, String paymentMethod) {
+        setId(id);
+        this.billingId = billingId;
+
+        this.paymentMethod = paymentMethod;
+    }
+
+    // Getters and setters
+    public int getBillingId() {
+        return billingId;
+    }
+
+    public void setBillingId(int billingId) {
+        this.billingId = billingId;
+    }
+
+    public double getAmountPaid() {
+        return amountPaid;
+    }
+
+    public void setAmountPaid(double amountPaid) {
+        this.amountPaid = amountPaid;
+    }
+
+    public String getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(String paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+}
